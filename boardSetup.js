e5e331105824d4e8b4cdd0f06a8ff3c6b2098a66
Kade Keith <keithkade@aol.com>
2014-03-24 22:42:05 -0500

    added rest of pieces and enabled default setup

--- boardSetup.js
+++ boardSetup.js
@@ -8,10 +8,10 @@ background.graphics.beginFill("#4E8154").drawRect(0, 0, boardWidth, boardHeight)
 stage.addChild(background);
 
 //highlight placable area
-var background = new createjs.Shape();
-background.graphics.beginFill("#3366FF").drawRect(0, 120, boardWidth, 180);
-//opacity
-stage.addChild(background);
+var placable = new createjs.Shape();
+placable.graphics.beginFill("#3366FF").drawRect(0, 120, boardWidth, 180);
+placable.alpha = .5;
+stage.addChild(placable);
 
 //draw grid
 for (var x = 0; x <= boardWidth; x += 60) {
@@ -25,20 +25,6 @@ for (var y = 0; y <= boardHeight; y += 60) {
 	line.graphics.beginFill("#000000").drawRect(0, y, boardWidth, 1);
     stage.addChild(line);
 }
-
-//create draggable important thing
-var square = new createjs.Shape();
-square.graphics.beginFill("red").drawRect(0, 0, 55, 55);
-var label = new createjs.Text("Important \n Thing", "10px Arial", "#FFFFFF");
-label.textAlign = "center";
-label.x = 28;
-label.y = 15;
-var importantThingDragger = new createjs.Container();
-importantThingDragger.x = 2;
-importantThingDragger.y = 482;
-importantThingDragger.addChild(square, label);
-stage.addChild(importantThingDragger);
-
 
 function addDragAndDrop (toDrag){
 	//move piece on drag			
@@ -68,6 +54,232 @@ function addDragAndDrop (toDrag){
 	});
 }
 
+//create draggable important thing
+var squareIT = new createjs.Shape();
+squareIT.graphics.beginFill("blue").drawRect(0, 0, 57, 57);
+var labelIT = new createjs.Text("Important \nThing", "10px Arial", "#FFFFFF");
+labelIT.textAlign = "center";
+labelIT.x = 28;
+labelIT.y = 15;
+var importantThingDragger = new createjs.Container();
+importantThingDragger.x = 2;
+importantThingDragger.y = 422;
+importantThingDragger.addChild(squareIT, labelIT);
+stage.addChild(importantThingDragger);
 addDragAndDrop(importantThingDragger);
 
+//create draggable trap1
+var squareT1 = new createjs.Shape();
+squareT1.graphics.beginFill("blue").drawRect(0, 0, 57, 57);
+var labelT1 = new createjs.Text("Trap - T", "10px Arial", "#FFFFFF");
+labelT1.textAlign = "center";
+labelT1.x = 28;
+labelT1.y = 15;
+var trap1Dragger = new createjs.Container();
+trap1Dragger.x = 62;
+trap1Dragger.y = 422;
+trap1Dragger.addChild(squareT1, labelT1);
+stage.addChild(trap1Dragger);
+addDragAndDrop(trap1Dragger);
+
+//create draggable trap2
+var squareT2 = new createjs.Shape();
+squareT2.graphics.beginFill("blue").drawRect(0, 0, 57, 57);
+var labelT2 = new createjs.Text("Trap - T", "10px Arial", "#FFFFFF");
+labelT2.textAlign = "center";
+labelT2.x = 28;
+labelT2.y = 15;
+var trap2Dragger = new createjs.Container();
+trap2Dragger.x = 122;
+trap2Dragger.y = 422;
+trap2Dragger.addChild(squareT2, labelT2);
+stage.addChild(trap2Dragger);
+addDragAndDrop(trap2Dragger);
+
+//create draggable archer
+var squareAr = new createjs.Shape();
+squareAr.graphics.beginFill("blue").drawRect(0, 0, 57, 57);
+var labelAr = new createjs.Text("Archer - 1", "10px Arial", "#FFFFFF");
+labelAr.textAlign = "center";
+labelAr.x = 28;
+labelAr.y = 15;
+var archerDragger = new createjs.Container();
+archerDragger.x = 182;
+archerDragger.y = 422;
+archerDragger.addChild(squareAr, labelAr);
+stage.addChild(archerDragger);
+addDragAndDrop(archerDragger);
+
+//create draggable mystic
+var squareMy = new createjs.Shape();
+squareMy.graphics.beginFill("blue").drawRect(0, 0, 57, 57);
+var labelMy = new createjs.Text("Mystic - 1", "10px Arial", "#FFFFFF");
+labelMy.textAlign = "center";
+labelMy.x = 28;
+labelMy.y = 15;
+var mysticDragger = new createjs.Container();
+mysticDragger.x = 242;
+mysticDragger.y = 422;
+mysticDragger.addChild(squareMy, labelMy);
+stage.addChild(mysticDragger);
+addDragAndDrop(mysticDragger);
+
+//create draggable rider1
+var squareR1 = new createjs.Shape();
+squareR1.graphics.beginFill("blue").drawRect(0, 0, 57, 57);
+var labelR1 = new createjs.Text("Rider - 2", "10px Arial", "#FFFFFF");
+labelR1.textAlign = "center";
+labelR1.x = 28;
+labelR1.y = 15;
+var rider1Dragger = new createjs.Container();
+rider1Dragger.x = 302;
+rider1Dragger.y = 422;
+rider1Dragger.addChild(squareR1, labelR1);
+stage.addChild(rider1Dragger);
+addDragAndDrop(rider1Dragger);
+
+//create draggable rider2
+var squareR2 = new createjs.Shape();
+squareR2.graphics.beginFill("blue").drawRect(0, 0, 57, 57);
+var labelR2 = new createjs.Text("Rider - 2", "10px Arial", "#FFFFFF");
+labelR2.textAlign = "center";
+labelR2.x = 28;
+labelR2.y = 15;
+var rider2Dragger = new createjs.Container();
+rider2Dragger.x = 362;
+rider2Dragger.y = 422;
+rider2Dragger.addChild(squareR2, labelR2);
+stage.addChild(rider2Dragger);
+addDragAndDrop(rider2Dragger);
+
+//create draggable assassin
+var squareAs = new createjs.Shape();
+squareAs.graphics.beginFill("blue").drawRect(0, 0, 57, 57);
+var labelAs = new createjs.Text("Assassin\n - A", "10px Arial", "#FFFFFF");
+labelAs.textAlign = "center";
+labelAs.x = 28;
+labelAs.y = 15;
+var assassinDragger = new createjs.Container();
+assassinDragger.x = 422;
+assassinDragger.y = 422;
+assassinDragger.addChild(squareAs, labelAs);
+stage.addChild(assassinDragger);
+addDragAndDrop(assassinDragger);
+
+//create draggable soldier1
+var squareS1 = new createjs.Shape();
+squareS1.graphics.beginFill("blue").drawRect(0, 0, 57, 57);
+var labelS1 = new createjs.Text("Soldier - 4", "10px Arial", "#FFFFFF");
+labelS1.textAlign = "center";
+labelS1.x = 28;
+labelS1.y = 15;
+var soldier1Dragger = new createjs.Container();
+soldier1Dragger.x = 2;
+soldier1Dragger.y = 482;
+soldier1Dragger.addChild(squareS1, labelS1);
+stage.addChild(soldier1Dragger);
+addDragAndDrop(soldier1Dragger);
+
+//create draggable soldier2
+var squareS2 = new createjs.Shape();
+squareS2.graphics.beginFill("blue").drawRect(0, 0, 57, 57);
+var labelS2 = new createjs.Text("Soldier - 4", "10px Arial", "#FFFFFF");
+labelS2.textAlign = "center";
+labelS2.x = 28;
+labelS2.y = 15;
+var soldier2Dragger = new createjs.Container();
+soldier2Dragger.x = 62;
+soldier2Dragger.y = 482;
+soldier2Dragger.addChild(squareS2, labelS2);
+stage.addChild(soldier2Dragger);
+addDragAndDrop(soldier2Dragger);
+
+//create draggable engineer1
+var squareE1 = new createjs.Shape();
+squareE1.graphics.beginFill("blue").drawRect(0, 0, 57, 57);
+var labelE1 = new createjs.Text("Engineer\n - 3", "10px Arial", "#FFFFFF");
+labelE1.textAlign = "center";
+labelE1.x = 28;
+labelE1.y = 15;
+var engineer1Dragger = new createjs.Container();
+engineer1Dragger.x = 122;
+engineer1Dragger.y = 482;
+engineer1Dragger.addChild(squareE1, labelE1);
+stage.addChild(engineer1Dragger);
+addDragAndDrop(engineer1Dragger);
+
+//create draggable engineer2
+var squareE2 = new createjs.Shape();
+squareE2.graphics.beginFill("blue").drawRect(0, 0, 57, 57);
+var labelE2 = new createjs.Text("Engineer\n - 3", "10px Arial", "#FFFFFF");
+labelE2.textAlign = "center";
+labelE2.x = 28;
+labelE2.y = 15;
+var engineer2Dragger = new createjs.Container();
+engineer2Dragger.x = 182;
+engineer2Dragger.y = 482;
+engineer2Dragger.addChild(squareE2, labelE2);
+stage.addChild(engineer2Dragger);
+addDragAndDrop(engineer2Dragger);
+
+//create draggable captain
+var squareCa = new createjs.Shape();
+squareCa.graphics.beginFill("blue").drawRect(0, 0, 57, 57);
+var labelCa = new createjs.Text("Captian - 5", "10px Arial", "#FFFFFF");
+labelCa.textAlign = "center";
+labelCa.x = 28;
+labelCa.y = 15;
+var captainDragger = new createjs.Container();
+captainDragger.x = 242;
+captainDragger.y = 482;
+captainDragger.addChild(squareCa, labelCa);
+stage.addChild(captainDragger);
+addDragAndDrop(captainDragger);
+
+//create draggable commander
+var squareCo = new createjs.Shape();
+squareCo.graphics.beginFill("blue").drawRect(0, 0, 57, 57);
+var labelCo = new createjs.Text("Commander\n - 6", "10px Arial", "#FFFFFF");
+labelCo.textAlign = "center";
+labelCo.x = 28;
+labelCo.y = 15;
+var commanderDragger = new createjs.Container();
+commanderDragger.x = 302;
+commanderDragger.y = 482;
+commanderDragger.addChild(squareCo, labelCo);
+stage.addChild(commanderDragger);
+addDragAndDrop(commanderDragger);
+
+function setup(){
+	importantThingDragger.x = 122;
+	importantThingDragger.y = 242;
+	archerDragger.x = 62;
+	archerDragger.y = 122;
+	commanderDragger.x = 182;
+	commanderDragger.y = 182;
+	trap1Dragger.x = 122;
+	trap1Dragger.y = 182;
+	trap2Dragger.x = 362;
+	trap2Dragger.y = 122;
+	captainDragger.x = 362;
+	captainDragger.y = 182;
+	engineer1Dragger.x = 62;
+	engineer1Dragger.y = 242;
+	engineer2Dragger.x = 242;
+	engineer2Dragger.y = 182;
+	soldier1Dragger.x = 62;
+	soldier1Dragger.y = 182;
+	soldier2Dragger.x = 302;
+	soldier2Dragger.y = 122;
+	assassinDragger.x = 302;
+	assassinDragger.y = 182;
+	mysticDragger.x = 242;
+	mysticDragger.y = 122;
+	rider1Dragger.x = 2;
+	rider1Dragger.y = 182;
+	rider2Dragger.x = 422;
+	rider2Dragger.y = 182;	
+	stage.update();
+}
+  			
 stage.update();
